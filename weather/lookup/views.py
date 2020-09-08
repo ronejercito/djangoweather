4def92dc3b02ddd68322a67cac4bf42da59d7e3e
Ron Ejercito <ron.ejercito@gmail.com>
2020-09-08 20:27:20 +0800

    second commit with weather API

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+# pylint: disable=unused-variable
+# pylint: enable=too-many-lines
+
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_requests = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=94080&distance=5&API_KEY=FB8713A4-7780-42D7-9B2D-AF57A058FF1B")
+
+    try:
+        api = json.loads(api_requests.content)
+    except Exception as e:
+        api = "Error..."
+
+    #http://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=94080&distance=10&API_KEY=FB8713A4-7780-42D7-9B2D-AF57A058FF1B
+    
+    return render(request, 'home.html', { 'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
